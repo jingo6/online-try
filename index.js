22cc65b91b026c69720ee8aa9e2821a751ccb98e
jingo6 <133201892+jingo6@users.noreply.github.com>
2023-08-24 13:25:27 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ window.onload = function(){
   toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
   })
-  }
-
+  };
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
