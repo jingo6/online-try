97e9e233b56584666d51e2dfd1f65c53df3d67b4
jingo6 <133201892+jingo6@users.noreply.github.com>
2023-08-25 12:19:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ window.onload = function(){
   }
 
 function sendEmail(){
-Email.send({
-    SecureToken: "c9462567-5507-472d-a434-880f005e2788 ",
-    To : 'jivan.jingo@gmail.com',
-    From : document.getElementByID("email").value,
-    Subject : "New contact form enquiry",
-    Body : " this is body"
-}).then(
-  message => alert("message sent")
-);
+    Email.send({
+        // SecureToken : "",
+
+        Host : "smtp.elasticemail.com",
+        Username : "jivan.jingo@gmail.com",
+        Password : "0BC134ABC8FC660E210CC14F377FE78695E5",
+        
+        To : 'jivan.jingo@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body :        "Name: "+document.getElementById("name").value
+                +"<br> Email: "+document.getElementById("email").value
+                +"<br> Phone: "+document.getElementById("phone").value
+                +"<br> Message: "+document.getElementById("message").value
+    }).then(
+      message => alert(message)
+    );
+
 }
