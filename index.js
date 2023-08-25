1ca3e8e49eb215384814b16ae45dbe3dacf10307
jingo6 <133201892+jingo6@users.noreply.github.com>
2023-08-25 09:17:46 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ window.onload = function(){
     navbarLinks.classList.toggle('active')
   })
   }
+}
+function sendEmail(){
+         Email.send({
+Email.send({
+    SecureToken: "c90feba5-68f2-47de-9e42-54ae30850e42",
+    To : 'jivan.jingo@gmail.com',
+    From : document.getElementsByID("email").value,
+    Subject : "New contact form enquiry",
+    Body : " this is body"
+}).then(
+  message => alert(message)
+);
+}
