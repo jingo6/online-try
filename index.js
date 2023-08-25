8b9aa5102131f1689a94b9b2dbf8706939438699
jingo6 <133201892+jingo6@users.noreply.github.com>
2023-08-25 10:24:04 +0545

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onload = function(){
 
 function sendEmail(){
 Email.send({
-    SecureToken: "c90feba5-68f2-47de-9e42-54ae30850e42",
+    SecureToken: "c9462567-5507-472d-a434-880f005e2788 ",
     To : 'jivan.jingo@gmail.com',
     From : document.getElementsByID("email").value,
     Subject : "New contact form enquiry",
