659a8f10d8cd375e389d482c79d405dd0e87f909
jingo6 <133201892+jingo6@users.noreply.github.com>
2023-08-25 08:45:47 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,4 @@ window.onload = function(){
   toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
   })
-  },
-function sendEmail(){
-         Email.send({
-Email.send({
-    SecureToken: "c90feba5-68f2-47de-9e42-54ae30850e42",
-    To : 'jivan.jingo@gmail.com',
-    From : document.getElementsByID("email").value,
-    Subject : "New contact form enquiry",
-    Body : " this is body"
-}).then(
-  message => alert(message)
-);
-}
+  }
